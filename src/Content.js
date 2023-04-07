eb4675fe2b80c792e9cfaef4eb0d2443d4b534ee
suman m <suman63616@gmail.com>
2023-04-07 15:54:51 +0530

    2nd commit

--- src/Content.js
+++ src/Content.js
@@ -1,26 +1,19 @@
-
-
-
-import React from 'react'
 import ItemList from './ItemList';
 
-const Content = ({items,handleCheck,handleDelete}) => {
-    
-
-    
-  return (
-    <>
-      {items.length?(
-          <ItemList
-          items={items}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-          />
-      ): (
-        <p style={{marginTop:'2rem'}}>your list is empty</p>
-      )}
-    </>
-  )
+const Content = ({ items, handleCheck, handleDelete }) => {
+    return (
+        <>
+            {items.length ? (
+                <ItemList
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
+            ) : (
+                <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+            )}
+        </>
+    )
 }
 
 export default Content
--- src/apiRequest.js
+++ src/apiRequest.js
@@ -1,15 +1,12 @@
-const apiRequest = async (url='',optionsObj=null,errMsg=null)=>{
-try {
-    const response= await fetch(url,optionsObj)
-    if(!response.ok) throw Error("please reload the app");
-
-}
-catch (err){
-    errMsg=err.message;
-}
-finally{
-return errMsg;
-}
+const apiRequest = async (url = '', optionsObj = null, errMsg = null) => {
+    try {
+        const response = await fetch(url, optionsObj);
+        if (!response.ok) throw Error('Please reload the app');
+    } catch (err) {
+        errMsg = err.message;
+    } finally {
+        return errMsg;
+    }
 }
 
 export default apiRequest;
